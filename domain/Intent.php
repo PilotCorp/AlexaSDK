1b46b731a34e77914d56cedc6f4f3bd2b5855daa
Jason Monsorno <JMonsorno@pilotpen.com>
2016-10-14 15:33:13 -0400

    JMonsorno Requiring Intent to be inherited

--- domain/Intent.php
+++ domain/Intent.php
@@ -1,7 +1,7 @@
 <?php
     class Intent {
         
-        function __construct($data) {
+        function __construct($data = null) {
             if (!$data) { return; }
             $this->Name = $data->name;
             foreach ($data->slots as $key => $val) {
@@ -14,6 +14,9 @@
 
         public function Run() {
             $intentName = $this->Name;
+            if (!(class_exists($intentName) && (new ReflectionClass($intentName))->getParentClass() && (new ReflectionClass($intentName))->getParentClass()->name == 'Intent')) {
+                return "Intent Action does not exist.";
+            }
             $intent = new $intentName();
             foreach (get_object_vars($this) as $key => $val) {
                 if (property_exists($val, 'value'))
