cfa3c15010e4ba2b10a3fa894d978d1caa631dea
Jason Monsorno <JMonsorno@pilotpen.com>
2016-10-14 14:39:33 -0400

    JMonsorno Encoding lower camelcase

--- Utils.php
+++ Utils.php
@@ -1,9 +1,26 @@
 <?php
     class Utils {
         public static function json_encode_no_nulls($obj) {
-            return preg_replace('/,\s*"[^"]+":null|"[^"]+":null,?/', '', json_encode($obj));
+            return preg_replace('/,\s*"[^"]+":null|"[^"]+":null,?/', '', json_encode(Utils::convert_to_array($obj)));
         }
 
+        public static function convert_to_array($obj, $recursive = true) {
+            $arr = get_object_vars($obj);
+                foreach($arr as $key => $val) {
+                    if ($recursive) {
+                        if (is_object($val)) {
+                            $arr[$key] = Utils::convert_to_array($val, $recursive);
+                        }
+                    }
+                    if (ctype_upper($key[0])) {
+                        $newKey = strtolower($key[0]) . substr($key, 1);
+                        $arr[$newKey] = $arr[$key];
+                        unset($arr[$key]);
+                    }
+                }
+            
+            return $arr;
+        }
 
         public static function json_decode_full(string $json, $obj) {
             $stdObj = json_decode($json);
